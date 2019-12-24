ed56c68f0c9903178b42ae46bb9de32bc81377da
ahmedElmaghr <elmaghraoui.ahmed@gmail.com>
2019-12-24 23:10:57 +0100

    Updates

---
+++ precache-manifest.849bb1b85ff52343b460c313eb53f859.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5e353500fa4e20f42d7d1b37228ae4",
+    "url": "/sunburst/index.html"
+  },
+  {
+    "revision": "0d42d597ea83330cd432",
+    "url": "/sunburst/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bddccf3c1cb94188b545",
+    "url": "/sunburst/static/css/main.b1f15495.chunk.css"
+  },
+  {
+    "revision": "0d42d597ea83330cd432",
+    "url": "/sunburst/static/js/2.06b60052.chunk.js"
+  },
+  {
+    "revision": "1fdd8477b82dbd0dc30ff62b40004fea",
+    "url": "/sunburst/static/js/2.06b60052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bddccf3c1cb94188b545",
+    "url": "/sunburst/static/js/main.3ff934bd.chunk.js"
+  },
+  {
+    "revision": "e23a5babf247e92b1b19",
+    "url": "/sunburst/static/js/runtime-main.95f3d7ea.js"
+  },
+  {
+    "revision": "f8ade2387d3bce4dd0d5c0dcf434b93a",
+    "url": "/sunburst/static/media/relations_medias_francais.f8ade238.tsv"
+  }
+]);
